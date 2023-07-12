0d477d27096ac1a52079d7255efe6ef8055782d1
Srikanth Kyatham <sreejntu63@gmail.com>
2023-07-12 09:45:04 -0500

    know_thread_name.py

---
+++ know_thread_name.py
@@ -0,0 +1,35 @@
+
+import queue
+from threading import Thread,current_thread
+
+def do_work(item):
+    try:
+        print (current_thread().getName())
+
+    except Exception as details:
+        print (details)
+        pass
+    print (item)
+    
+ 
+
+def worker():
+      while True:            
+        item=q.get()
+        do_work(item)
+        q.task_done()
+        
+q=queue.Queue()
+l=[13,26,77,99,101,4003]
+for item in l:
+      q.put(item)
+ 
+ 
+for i in range (20):
+    
+    t=Thread(target=worker,name="child"+str(i))
+    t.daemon=True
+    t.start()
+ 
+ 
+q.join()
